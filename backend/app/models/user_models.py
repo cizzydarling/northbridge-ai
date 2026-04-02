056db6f99b61b23e5353ca3707d942800d4c2a19
Cizzy Darling <YOUR_GITHUB_EMAIL>
2026-04-02 10:47:18 -0400

    Fix model definitions for User/Profile/Client

--- backend/app/models/user_models.py
+++ backend/app/models/user_models.py
@@ -8,18 +8,13 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
-
     role = Column(String, nullable=False, default="individual")
 
     plan = Column(String, nullable=False, default="free")
     subscription_status = Column(String, nullable=True)
     stripe_customer_id = Column(String, nullable=True, unique=True)
     stripe_subscription_id = Column(String, nullable=True, unique=True)
-
-    first_name = Column(String, nullable=True)
-    last_name = Column(String, nullable=True)
 
     profile = relationship("Profile", back_populates="user", uselist=False)
